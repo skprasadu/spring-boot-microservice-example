177c8a59bfcae1a883b6cd4815ff546e4b3bd161
Krishna Prasad <praskrishna@gmail.com>
2022-03-24 09:16:34 -0700

    modified build script

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-mvn clean install -f ./spring-boot-cloud-eureka-server\pom.xml
-mvn clean install -f ./spring-boot-cloud-zuul-routing\pom.xml
-mvn clean install -f ./spring-boot-cloud-eureka-user-service\pom.xml
-mvn clean install -f ./spring-boot-cloud-eureka-order-service\pom.xml
-mvn clean install -f ./spring-boot-cloud-authentication-service\pom.xml
+mvn clean package -f ./spring-boot-cloud-eureka-server/pom.xml  
+mvn clean package -f ./spring-boot-cloud-api-gateway-routing/pom.xml  
+mvn clean package -f ./spring-boot-cloud-eureka-user-service/pom.xml  
+mvn clean package -f ./spring-boot-cloud-eureka-order-service/pom.xml  
+mvn clean package -f ./spring-boot-cloud-eureka-notification-service/pom.xml  
+mvn clean package -f ./spring-boot-cloud-authentication-service/pom.xml 
 
 java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar  &
-java -jar spring-boot-cloud-zuul-routing/target/zuul-api-gateway.jar --server.port=8080  &
+java -jar spring-boot-cloud-api-gateway-routing/target/zuul-api-gateway.jar --server.port=8080  &
 java -jar spring-boot-cloud-eureka-user-service/target/user-service.jar --server.port=8181 &
 java -jar spring-boot-cloud-eureka-order-service/target/order-service.jar --server.port=8282 &
 java -jar spring-boot-cloud-authentication-service/target/authentication-service.jar --server.port=8383 &
+java -jar spring-boot-cloud-eureka-notification-service/target/notification-service.jar --server.port=8484
